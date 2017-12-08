66037643cd8e1bcef172c24f33ee1b6dc82e8161
Edgar F. Black <efblack2@illinois.edu>
2017-12-08 15:01:05 -0600

    running scripts

--- Mpi+Mpi/runTestMPI+MPI.sh
+++ Mpi+Mpi/runTestMPI+MPI.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-if [ "$#" -ne 2 ] 
+if [ "$#" -ne 3 ] 
 then
-  echo "Usage: $0 numberOfIterations  compiler"
+  echo "Usage: $0  programName numberOfIterations  compiler"
   exit 1
 fi
 
-nloops=2
+tempFilename='anyTempFileNameWillWork.txt'
+
+program=$1
+outputFilename=$program'.txt'
+
+#echo $program
+#echo $outputFilename
+
+nloops=5
 np=`grep -c ^processor /proc/cpuinfo`
 
+rm -f $tempFilename
 
-rm -f Mpi+MPI_Result.txt
 for i in  `seq 1 $np`; do
     for j in  `seq 1 $nloops`; do
         echo number of processors: $i, run number: $j 
-        mpiexec -n $i laplace_MPI+MPI $1 | grep Total >>  Mpi+MPI_Result.txt
+        mpiexec -n $i $program $2 | grep Total >>  $tempFilename
     done
 done
 
-mkdir -p ../../../plots/PureShareMemory/$(hostname)/$2
-cat Mpi+MPI_Result.txt | awk '{}{print $5, $8}{}' | awk '{Prod[$1]++; min[$1]=Prod[$1]==1||min[$1]>$2?$2:min[$1]} END{ for (var in Prod) printf "%s processors: the min is %f\n", var,min[var]}'  | sort -n   > ../../../plots/PureShareMemory/$(hostname)/$2/p6_MPI_sm.txt
+mkdir -p ../../plots/MPI+MPI/$(hostname)/$3
 
-rm Mpi+MPI_Result.txt
-#rm RunHistory.dat
+cat $tempFilename | awk 'BEGIN{} {print $4,$6, $11, $17} END{}' | awk '{Prod[$2]++; min[$2]=Prod[$2]==1||min[$2]>$4?$4:min[$2]} END{ for (var in Prod) printf "%s processors: the min is %f\n", var,min[var]}' | sort -n  > ../../plots/MPI+MPI/$(hostname)/$3/$outputFilename
+
+rm $tempFilename
 
--- Mpi/runTestMPI.sh
+++ Mpi/runTestMPI.sh
@@ -19,16 +19,15 @@ np=`grep -c ^processor /proc/cpuinfo`
 rm -f $tempFilename
 
 for i in  `seq 1 $np`; do
-    export OMP_NUM_THREADS=$i
     for j in  `seq 1 $nloops`; do
         echo number of processors: $i, run number: $j 
-        mpiexec -n 1 $program $2 | grep Total >>  $tempFilename
+        mpiexec -n $i $program $2 | grep Total >>  $tempFilename
     done
 done
 
 mkdir -p ../../plots/MPI+MPI/$(hostname)/$3
 
-cat $tempFilename | awk 'BEGIN{} {print $4,$10, $13, $14} END{}' | awk '{Prod[$2]++; min[$2]=Prod[$2]==1||min[$2]>$4?$4:min[$2]} END{ for (var in Prod) printf "%s threads: the min is %f\n", var,min[var]}' | sort -n  > ../../plots/MPI+MPI/$(hostname)/$3/$outputFilename
+cat $tempFilename | awk 'BEGIN{} {print $4,$7, $8, $11} END{}' | awk '{Prod[$2]++; min[$2]=Prod[$2]==1||min[$2]>$4?$4:min[$2]} END{ for (var in Prod) printf "%s processors: the min is %f\n", var,min[var]}' | sort -n  > ../../plots/MPI+MPI/$(hostname)/$3/$outputFilename
 
 rm $tempFilename
 
