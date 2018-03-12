537927b628c01f5cf4fdacdee8aedd79bf83c359
Edgar F. Black <efblack2@illinois.edu>
2018-03-12 16:24:16 -0500

    commenting out compiling lines  of pure OpenMP version

--- compileAll.sh
+++ compileAll.sh
@@ -3,21 +3,19 @@
 
 mkdir -p  Mpi+Mpi/buildGnu
 mkdir -p  Mpi+OpenMP/buildGnu
-mkdir -p  OpenMP/buildGnu
+#mkdir -p  OpenMP/buildGnu
 
 mkdir -p  Mpi+Mpi/buildIntel
 mkdir -p  Mpi+OpenMP/buildIntel
-mkdir -p  OpenMP/buildIntel
+#mkdir -p  OpenMP/buildIntel
 
 mkdir -p  Mpi+Mpi/buildPgi
 mkdir -p  Mpi+OpenMP/buildPgi
-mkdir -p  OpenMP/buildPgi
-
-
+#mkdir -p  OpenMP/buildPgi
 
 
 cd OpenMP/buildGnu
-cmake .. ; make clean; make
+#cmake .. ; make clean; make
 cd ../../Mpi+OpenMP/buildGnu
 cmake .. ; make clean; make
 cd ../../Mpi+Mpi/buildGnu
@@ -30,7 +28,7 @@ source setIcc intel64
 source setImpi
 
 cd OpenMP/buildIntel
-cmake .. ; make clean; make
+#cmake .. ; make clean; make
 cd ../../Mpi+OpenMP/buildIntel
 cmake .. ; make clean; make
 cd ../../Mpi+Mpi/buildIntel
@@ -43,7 +41,7 @@ source setPgi 18.1
 source setPgiMpi 18.10
 
 cd OpenMP/buildPgi
-cmake .. ; make clean; make
+#cmake .. ; make clean; make
 cd ../../Mpi+OpenMP/buildPgi
 cmake .. ; make clean; make
 cd ../../Mpi+Mpi/buildPgi
