11a40582d9d2a870cd151c1aeb70a20c5bdcbef7
Edgar F. Black <efblack2@illinois.edu>
2018-03-19 14:54:04 -0500

    adding threadsPerCore variable

--- Mpi+Mpi/runTestMPI+MPI.sh
+++ Mpi+Mpi/runTestMPI+MPI.sh
@@ -11,7 +11,8 @@ outputFilename='laplace_MPI+MPI.txt'
 nloops=3
 npt=`grep -c ^processor /proc/cpuinfo`
 sockets=`lscpu | grep Socket | awk '{}{print $2}{}'`
-np="$(($npt / 1))"
+tpc=`lscpu | grep -i thread | awk '{}{print $4}{}'`
+np="$(($npt / $tpc))"
 npps="$(($np / $sockets))"
 npm1="$(($np - 1))"
 
