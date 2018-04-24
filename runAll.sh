1c0b4197fb905af38167516896fd5c540a214cf9
Edgar Black <efblack2@koelsch.cs.illinois.edu>
2018-04-24 13:08:36 -0500

    including mpi_fenec

--- runAll.sh
+++ runAll.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 cd Mpi+OpenMP/buildGnu
-make clean; make
-../runTest.sh 1000 gnu
+#make clean; make
+#../runTest.sh 1000 gnu
 
 cd ../../Mpi+Mpi/buildGnu
-make clean; make
-../runTest.sh 1000 gnu
+#make clean; make
+#../runTest.sh 1000 gnu
+
+cd ../../Mpi+Mpi_fence/buildGnu
+#make clean; make
+#../runTest.sh 1000 gnu
 
 cd ../../
 
@@ -14,10 +18,14 @@ source setIcc intel64
 source setImpi
 
 cd Mpi+OpenMP/buildIntel
-make clean; make
-../runTest.sh 1000 intel
+#make clean; make
+#../runTest.sh 1000 intel
 
 cd ../../Mpi+Mpi/buildIntel
+#make clean; make
+#../runTest.sh 1000 intel
+
+cd ../../Mpi+Mpi_fence/buildIntel
 make clean; make
 ../runTest.sh 1000 intel
 
@@ -28,12 +36,16 @@ source setPgiMpi 18.3
 
 
 cd Mpi+OpenMP/buildPgi
-make clean; make
-../runTest.sh 1000 pgi
+#make clean; make
+#../runTest.sh 1000 pgi
 
 cd ../../Mpi+Mpi/buildPgi
-make clean; make
-../runTest.sh 1000 pgi
+#make clean; make
+#../runTest.sh 1000 pgi
+
+cd ../../Mpi+Mpi_fence/buildPgi
+#make clean; make
+#../runTest.sh 1000 pgi
 
 cd ../../
 
