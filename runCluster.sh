526f30f6925e37d6b44c0f04783ceddda15a279d
Edgar Black <efblack2@illinois.edu>
2018-06-22 14:01:56 -0500

    cluster run script

---
+++ runCluster.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd Mpi+OpenMP/buildGnu
+make clean; make
+../runNodesTest.sh 1000 gnu
+
+cd ../../Mpi+Mpi/buildGnu
+make clean; make
+../runNodesTest.sh 1000 gnu
+
+cd ../../Mpi/buildGnu
+make clean; make
+../runNodesTest.sh 1000 gnu
+
+#cd ../../Mpi+Mpi_fence/buildGnu
+#make clean; make
+#../runTest.sh 1000 gnu
+
+cd ../../
+
+source setIcc intel64
+source setImpi
+
+cd Mpi+OpenMP/buildIntel
+make clean; make
+../runNodesTest.sh 1000 intel
+
+cd ../../Mpi+Mpi/buildIntel
+make clean; make
+../runNodesTest.sh 1000 intel
+
+cd ../../Mpi/buildIntel
+make clean; make
+../runNodesTest.sh 1000 intel
+
