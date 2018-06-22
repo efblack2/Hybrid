b010186a21d38877cf29c4ec5f9e824efef48fe8
Edgar Black <efblack2@illinois.edu>
2018-06-22 16:08:11 -0500

    adding pgi compiler to cluster script

--- runCluster.sh
+++ runCluster.sh
@@ -33,3 +33,27 @@ cd ../../Mpi/buildIntel
 make clean; make
 ../runNodesTest.sh 1000 intel
 
+cd ../../
+
+source setPgi 18.x
+source setPgiMpi 18.x
+
+
+cd Mpi+OpenMP/buildPgi
+make clean; make
+../runNodesTest.sh 1000 pgi
+
+cd ../../Mpi+Mpi/buildPgi
+make clean; make
+../runNodesTest.sh 1000 pgi
+
+cd ../../Mpi/buildPgi
+make clean; make
+../runNodesTest.sh 1000 pgi
+
+#cd ../../Mpi+Mpi_fence/buildPgi
+#make clean; make
+#../runTest.sh 1000 pgi
+
+cd ../../
+
