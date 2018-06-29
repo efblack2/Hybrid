a76c85c8d37b14ab4a7c7bbc3e7265ad4899dfa3
Edgar Black <efblack2@koelsch.cs.illinois.edu>
2018-06-29 08:45:38 -0500

    script for ploting cluster updated

--- plots/createPlotHybrid.sh
+++ plots/createPlotHybrid.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 if [ "$#" -lt 1 ]
 then
-  echo "Usage: $0  computer"
+  echo "Usage: $0  compiler"
   exit 1
 fi
 
-cd $1/gnu
+cd cluster/$1
 paste laplace_MPI.txt laplace_MPI+OpenMP.txt laplace_MPI+MPI.txt  > laplace.txt
-
 cd ../..
 
 gnuplot -c plotCluster.gnp $1  
+
+
 
 rm `find . -name  laplace.txt`
 
